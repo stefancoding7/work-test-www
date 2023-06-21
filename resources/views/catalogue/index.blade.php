18ffa462918cd9ec361728c42ff6e995ef0217eb
Stefan Caky <stefan.caky777@gmail.com>
2023-06-21 19:06:34 +0100

    upda

--- resources/views/catalogue/index.blade.php
+++ resources/views/catalogue/index.blade.php
@@ -33,13 +33,15 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    @foreach($products as $product}
-                    <div>
-                        <p>{{$product->sku}}</p>
-                        <p>{{$product->description}}</p>
-                        <p>{{$product->price}}</p>
-                    </div>
-                    @endforeach
+                    @if($products->count() > 0)
+                        @foreach($products as $product)
+                        <div>
+                            <p>{{$product->sku}}</p>
+                            <p>{{$product->description}}</p>
+                            <p>{{$product->price}}</p>
+                        </div>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
