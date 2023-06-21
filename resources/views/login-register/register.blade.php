845ebb7863536abbb7d504b3a382de168c55722f
Stefan Caky <stefan.caky777@gmail.com>
2023-06-21 19:54:14 +0100

    upda

--- resources/views/login-register/register.blade.php
+++ resources/views/login-register/register.blade.php
@@ -3,9 +3,9 @@
 
 <div class="row">
     <div class="col-md-6 col-6">
-        <div class="card own-card shadow-lg ">
-            <div class="card-body text-center">
-                <h2>Login</h2>
+        <div class="card own-card">
+            <div class="card-body">
+                <h2>Register</h2>
                 <form>
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email address</label>
@@ -16,16 +16,18 @@
                         <label for="exampleInputPassword1" class="form-label">Password</label>
                         <input type="password" class="form-control" id="exampleInputPassword1">
                     </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
                     
-                    <button type="submit" class="btn btn-primary rounded-pill" style="width: 100px">Login</button>
+                    <button type="submit" class="btn btn-primary rounded-pill">Register</button>
                 </form>
             </div>
         </div>
     </div>
     <div class="col-md-6 col-6">
-        <div class="login-side">
-
-        </div>
+        
     </div>
 </div>
 
