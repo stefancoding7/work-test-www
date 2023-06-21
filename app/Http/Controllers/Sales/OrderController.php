4b57b320125ee82950e015cb5ab5edea0e5cff31
Stefan Caky <stefan.caky777@gmail.com>
2023-06-21 22:05:17 +0100

    upda

--- app/Http/Controllers/Sales/OrderController.php
+++ app/Http/Controllers/Sales/OrderController.php
@@ -19,7 +19,33 @@ class OrderController extends Controller
 
     public function saveOrder(Request $request)
     {
-        dd($request);
+
+        $user = auth()->user();
+        $total = Cart::total();
+
+
+        $order = new Order;
+        $order->customer_id = $user->id;
+        $order->price = $total;
+        $order->delivery_address = $this->saveAddress($request, $user);
+        
+        
+
         return view('order.thanks')->with('order', $order);
+    }
+
+    public function saveAddress($request, $user)
+    {
+        
+        $shipp_address = new \stdClass();
+        $shipp_address->full_name = $request->full_name;
+        $shipp_address->email = $user->email;
+        $shipp_address->country = $request->country;
+        $shipp_address->city = $request->city;
+        $shipp_address->postcode = $request->postcode;
+        $shipp_address->address = $request->address;
+        $shhip = json_encode($shipp_address, true);
+        return $shhip;
+        
     }
 }
