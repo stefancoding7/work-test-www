f6414b01cd185d7a45ddd0799d849eb9883a19e9
Stefan Caky <stefan.caky777@gmail.com>
2023-06-21 20:22:50 +0100

    upda

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,9 +50,7 @@
                         </li>
                     @endauth
                     
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
+                    
                 </ul>
                 </div>
             </div>
